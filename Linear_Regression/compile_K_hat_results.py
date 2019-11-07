70af875abd86271eadf5d09c645fc231aa406757
Akash Kumar Dhaka <akash.uee@gmail.com>
2019-11-07 20:02:39 +0200

    small chnages to compiling results script

--- Linear_Regression/compile_K_hat_results.py
+++ Linear_Regression/compile_K_hat_results.py
@@ -2,9 +2,23 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
-algo_name = 'mf'
-N = 5000
+parser = argparse.ArgumentParser()
+parser.add_argument('--algorithm', '-a', type=int, default=1)
+parser.add_argument('--N', '-n', type=int, default=1000)
+parser.add_argument('--iters', '-i', type=int, default=80000)
+
+args = parser.parse_args()
+N = args.N
+
+if args.algorithm ==1:
+    algo = 'meanfield'
+    algo_name = 'mf'
+elif args.algorithm ==2:
+    algo = 'fullrank'
+    algo_name = 'fr'
+
 
 a1 = np.load('K_hat_linear_independent_'+ algo_name + '_' + str(N) + 'N.npy')
 a2 = np.load('K_hat_linear_correlated_'+ algo_name + '_' + str(N) + 'N.npy')
